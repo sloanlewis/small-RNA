698e6386880431ec861b3e76fe96706d4c2b8177
suhaschandra <ssureshc@uci.edu>
2018-03-05 09:24:14 -0800

    Create small_RNA_alignment.R

---
+++ small_RNA_alignment.R
@@ -0,0 +1,15 @@
+#SystemPipeR template for read alignment using bowtie with modified parameters.
+library(systemPipeR)
+library(GenomicFeatures)
+
+#Read the targets file
+targets <- read.delim("targets.txt", comment.char = "#")
+#Check if targets file is correct
+targets
+#Create args
+args <- systemArgs(sysma="tophat.param", mytargets="targets.txt")
+moduleload(modules(args))
+
+#Check if alignment files exist
+file.exists(outpaths(args))
+
