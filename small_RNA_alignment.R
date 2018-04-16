1764148c699737502ec93249d7bbbf59f5be47f6
suhaschandra <ssureshc@uci.edu>
2018-04-16 11:44:20 -0700

    Update small_RNA_alignment.R

--- small_RNA_alignment.R
+++ small_RNA_alignment.R
@@ -14,7 +14,7 @@ moduleload(modules(args))
 file.exists(outpaths(args))
 
 #Assign resources. Note this assigns 1Gb of Ram per core. If ncpus is 4 then this will amount to 4Gb total
-resources <- list(walltime="20:00:00", ntasks=1, ncpus=cores(args), memory="1G") 
+resources <- list(walltime="20:00:00", ntasks=1, ncpus=cores(args), memory="5G") 
 
 #Submit jobs on the cluster
 reg <- clusterRun(args, conffile=".BatchJobs.R", template="slurm.tmpl", Njobs=18, runid="01", resourceList=resources)
